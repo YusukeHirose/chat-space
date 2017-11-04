ba937500c83973e7d809e79c1e05315ecb8cb7c2
kdoebr21 <kodeberic@yahoo.co.jp>
2017-11-04 13:53:42 +0900

    devise-function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+      # added_attrs = [ :name, :email, :password, :password_confirmation ]
+      # devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      # devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      # devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      # devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+      # sign_inのときに、nameも許可する
+      devise_parameter_sanitizer.permit(:sign_in, keys:[:name])
+    # sign_upのときに、nameも許可する
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+    #account_updateのときに、nameも許可する
+      devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
 
   def edit
-    
+
   end
 
   def update
-    
+    current_user.update(create_params)
+    redirect_to :root
+  end
+
+  def create_params
+    params.require(:user).permit(:name, :email)
   end
 
 end
