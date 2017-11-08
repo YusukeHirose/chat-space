06f8227ba90063bcb8fa2bf14c074a456df94d73
kdoebr21 <kodeberic@yahoo.co.jp>
2017-11-08 21:54:22 +0900

    flash_create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,10 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(update_params)
-      redirect_to :root
-    elsif
-      render action: :edit
+      redirect_to :root, notice: "編集完了しました。"
     else
-
+      flash.now[:alert] = "編集に失敗しました。"
+      render action: :edit
     end
   end
 
