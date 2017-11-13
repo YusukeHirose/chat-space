bd46b18ab83f1d5e4d844e5d9bb227cb16d24b4f
kdoebr21 <kodeberic@yahoo.co.jp>
2017-11-13 19:21:14 +0900

    groupfunc 修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # resources :users, only: [:show, :edit, :update]
   resources :users, only: [:edit, :update]
-  resources :groups, except: [:destroy]
+  resources :groups, except: [:show, :destroy]
   root 'messages#index'
 
 end
