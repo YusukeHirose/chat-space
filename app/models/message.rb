246d5d1045cad128118a0b685e7df02c504a51ed
kdoebr21 <kodeberic@yahoo.co.jp>
2018-02-12 15:16:00 +0900

    modelにvalidate追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,11 @@ class Message < ApplicationRecord
   belongs_to :user
 
   mount_uploader :image, ImageUploader
+
+  validates :bory_or_image, presence: true
+
+  private
+  def bory_or_image
+    body.presence or image.presence
+  end
 end
