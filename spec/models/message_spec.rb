896ed9d30613132d106cd37c0983ae2ac8907868
kdoebr21 <kodeberic@yahoo.co.jp>
2018-02-17 14:32:55 +0900

    テスト文変更

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,34 +3,34 @@ require 'rails_helper'
 RSpec.describe Message, type: :model do
   describe '#create' do
     context 'save' do
-      it 'is valid with body' do
+      it 'is valid for message only with body' do
         expect(build(:message, image: nil)).to be_valid
       end
 
-      it 'is valid with image' do
+      it 'is valid for message only widh image' do
         expect(build(:message, body: nil)).to be_valid
       end
 
-      it 'is valid with body and image' do
+      it 'is valid for message with body and image' do
         expect(build(:message)).to be_valid
       end
     end
 
     context 'cant save' do
 
-      it 'is invalid without group_id' do
+      it 'is invalid for message without group_id' do
         message = build(:message, group_id: nil)
         message.valid?
         expect(message.errors[:group]).to include('を入力してください')
       end
 
-      it 'is invalid without user_id' do
+      it 'is invalid for message without user_id' do
         message = build(:message, user_id: nil)
         message.valid?
         expect(message.errors[:user]).to include('を入力してください')
       end
 
-      it 'is invalid without body and image' do
+      it 'is invalid for message without body and image' do
         message = build(:message, body: nil, image: nil)
         message.valid?
         expect(message.errors[:body_or_image]).to include("を入力してください。")
