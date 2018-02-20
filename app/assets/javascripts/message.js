d52452b5f218fbf24989700a691bce7dafc9e244
kdoebr21 <kodeberic@yahoo.co.jp>
2018-02-20 12:58:49 +0900

    write Ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,17 @@
 $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
+    var formData = new FormData(this);
+    //リクエスト送信先のURLを定義
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      //dataに指定したオブジェクトをクエリ文字列に変換
+      processData: false,
+      contentType: false
+    })
   })
 })
