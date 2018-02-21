af21e8eedec9bc1b8d46ce09e0dcaf0564934ce8
kdoebr21 <kodeberic@yahoo.co.jp>
2018-02-21 13:11:32 +0900

    details modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   protect_from_forgery with: :exception
 
   def configure_permitted_parameters
@@ -18,4 +17,5 @@ class ApplicationController < ActionController::Base
     #account_updateのときに、nameも許可する
       devise_parameter_sanitizer.permit(:account_update, keys:[:name])
   end
+
 end
