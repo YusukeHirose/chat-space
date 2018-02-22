6391b51107ad8a10545b03132e3ed189a993dbbd
kdoebr21 <kodeberic@yahoo.co.jp>
2018-02-23 01:01:43 +0900

    rename:  index.json.jbuilder from search.json.jbuilder

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -7,6 +7,7 @@ $(function() {
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
+    console.log(html)
     search_list.append(html);
   }
 
@@ -22,7 +23,7 @@ $(function() {
 
     $.ajax({
       type: 'GET',
-      url: '/users/index',
+      url: '/users',
       data: { keyword: input },
       dataType: 'json'
     })
