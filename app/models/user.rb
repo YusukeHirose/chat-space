b75cbd45516134fd0066a8ef11f8145b7c4d35b2
kdoebr21 <kodeberic@yahoo.co.jp>
2018-03-02 00:55:32 +0900

    change argument to current_user from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   has_many :members, dependent: :destroy
   has_many :messages
 
-  scope :exclude_currentUser, -> (user) { where.not(id: user.id).limit(20) }
+  scope :exclude_currentUser, -> (current_user) { where.not(id: current_user.id).limit(20) }
 
 end
